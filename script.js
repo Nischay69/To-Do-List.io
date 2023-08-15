8d044bb4a131f6fa166426cb1289f73c25308e1d
Trishan Wagle <mailtotrishan@gmail.com>
2023-08-15 12:44:41 +0545

    fix: code cleanups and responsive

--- script.js
+++ script.js
@@ -2,38 +2,39 @@ const inputBox = document.querySelector("#input-box");
 const listContainer = document.querySelector("#list-container");
 const addButton = document.querySelector(".list-add-button");
 
-function addTask(e) {
+const showData = () => {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+showData();
+
+const addTask = () => {
   if (inputBox.value == "") {
     alert("You must Write Something");
   } else {
-    let li = document.createElement("li");
+    const li = document.createElement("li");
     li.innerHTML = inputBox.value;
     listContainer.appendChild(li);
-    let span = document.createElement("span");
+    const span = document.createElement("span");
     span.innerHTML = "\u00d7";
     li.appendChild(span);
   }
   inputBox.value = "";
   saveData();
 }
-listContainer.addEventListener(
-  "click",
-  function (e) {
-    if (e.target.tagName == "LI") {
-      e.target.classList.toggle("checked");
-      saveData();
-    } else if (e.target.tagName == "SPAN") {
-      e.target.parentElement.remove();
-      saveData();
-    }
-  },
-  false
-);
-function saveData() {
+
+const saveData = () => {
   localStorage.setItem("data", listContainer.innerHTML);
 }
-function showData() {
-  listContainer.innerHTML = localStorage.getItem("data");
+
+const handleCheck = (e) => {
+  if (e.target.tagName == "LI") {
+    e.target.classList.toggle("checked");
+    saveData();
+  } else if (e.target.tagName == "SPAN") {
+    e.target.parentElement.remove();
+    saveData();
+  }
 }
-showData();
+
+listContainer.addEventListener("click", handleCheck);
 addButton.addEventListener("click", addTask);
